5b5aacd995d7500a945dccc45a047d0dab3138c7
prakashp007 <ppb5787@gmail.com>
2023-06-27 22:36:55 +0530

    Data type Demo

---
+++ DataType.dart
@@ -0,0 +1,29 @@
+void main(){
+  var number = 1; //not explicitly define the data type
+  int numberRe = 2 ; //explicitlh define the data type
+
+
+
+  print(number + numberRe);
+
+  number = 4;
+
+  print(number + numberRe);
+  var stringName = " Name";
+  String stringNameTwo = "Hello";
+
+  print(stringName + stringNameTwo);
+
+  var hiteshPer = 99.9;
+  var harshPer = 90.00;
+
+  print(hiteshPer);
+  print(harshPer);
+
+
+  var isValid = true;
+  bool orNot = false;
+
+  print(isValid);
+  print(orNot);
+}
