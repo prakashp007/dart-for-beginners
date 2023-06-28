b6e77ce1dc17603f7b344eb8577dce844c713a2b
prakashp007 <ppb5787@gmail.com>
2023-06-28 22:46:18 +0530

    control flow

---
+++ IfElseIfElseledder.dart
@@ -0,0 +1,38 @@
+void main(){
+  var  salary  = 20000;
+  //if else 
+  if(salary > 30000){
+    print("Congratulation");
+  }else {
+    print("Work hard bro ");
+  }
+
+  //if else ledder
+
+  var mark = 50;
+
+  if(mark >= 90 && mark <= 100){
+    print("sabash beta");
+  }else if(mark >= 80 && mark < 90){
+    print("sahi hai");
+  }else if(mark >= 70 && mark < 80){
+    print("Addmission mil jayega!!!");
+  }else if(mark > 60 && mark < 70){
+    print("jitna likha tha utane aaye");
+  }else if(mark>=50 && mark<60){
+    print("upon average student hai bhai");
+  }else if(mark >= 40 && mark < 50){
+    print("kuch padha hi nahi tha ");
+  }else if(mark >= 30 && mark <40){
+    print("apne ko to pass ho na tha ");
+  }else if(mark >=0 && mark <30){
+    print("Fail...");
+  }else {
+    print("Invalid");
+  }
+
+
+
+
+
+}
