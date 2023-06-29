aed98117e7ebbff52d117b2b812727916b86ba92
prakashp007 <ppb5787@gmail.com>
2023-06-29 22:08:12 +0530

    Switch Case Demo

---
+++ SwitchCase.dart
@@ -0,0 +1,24 @@
+void main(){
+    //Switch case statements : Applicable for only Int or string
+
+
+    String grade = 'A';
+    switch(grade){
+      case 'A':
+        print("Grade A");
+        break;
+      case 'B':
+        print("Grade B");
+        break;
+      case 'C':
+        print("Grade C");
+        break;
+      case 'D':
+        print("Grade D");
+        break;
+      default:
+        print("In Valid Case");
+        
+    } 
+
+}
