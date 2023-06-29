0c5101610368e0bc7195a4c5f6b6c76605af20dc
prakashp007 <ppb5787@gmail.com>
2023-06-29 23:06:53 +0530

    break statement with labels

---
+++ Break.dart
@@ -0,0 +1,14 @@
+void main(){
+  //Break keyword
+  //using labels
+
+
+  myhOuterLoop: for(int i = 1;i<=3;i++){
+    for(int j = 1;j<=3;j++){
+      print("$i $j");
+      if(i==2 && j==2){
+        break myhOuterLoop;
+      }
+    }
+  }
+}
