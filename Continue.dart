7ff62e7c0104207ce70b419b501b8742bc8bddd5
prakashp007 <ppb5787@gmail.com>
2023-06-29 23:18:47 +0530

    continue with labels

---
+++ Continue.dart
@@ -0,0 +1,13 @@
+void main(){
+  //continue 
+  //with labels 
+  
+ outer: for(int i=1;i<=3;i++){
+    for(int j=1;j<=3;j++){
+      if(i==2 && j==2){
+        continue outer;
+    }
+    print('$i $j');
+    }
+  }
+}
