18c3b49fc5a20b8945b53df421acc5c7249ce243
prakashp007 <ppb5787@gmail.com>
2023-07-20 22:05:52 +0530

    collection demo

---
+++ Collcection/GrowableList.dart
@@ -0,0 +1,20 @@
+void main() {
+  List<int> number = [];
+
+  number.add(1);
+  number.add(2);
+  number.add(2);
+  number.add(2);
+  number.add(2);
+  number.add(2);
+  number.add(2);
+  number.add(2);
+  number.add(2);
+  number.add(2);
+
+  print(number);
+
+  number.forEach((element) {
+    print(element);
+  });
+}
--- Collcection/ListDemo.dart
+++ Collcection/ListDemo.dart
@@ -7,25 +7,25 @@ void main() {
 //  Objectives
 //  1. Fixed - length list
 //fixed length
- List? numberOfList = List.filled(5, int,growable: false);
- numberOfList[0] = 1;
- numberOfList[1] = 2;
- numberOfList[2] = 3;
- numberOfList[3] = 4;
- numberOfList[4] = 5;
+  List? numberOfList = List.filled(5, int, growable: false);
+  numberOfList[0] = 1;
+  numberOfList[1] = 2;
+  numberOfList[2] = 3;
+  numberOfList[3] = 4;
+  numberOfList[4] = 5;
 
- print(numberOfList);
-for(var i in numberOfList){
-  print(i);
-}
+  print(numberOfList);
+  for (var i in numberOfList) {
+    print(i);
+  }
 
-print("\n");
-numberOfList.forEach((element) { 
-  print(element);
-});
-print("\n");
+  print("\n");
+  numberOfList.forEach((element) {
+    print(element);
+  });
+  print("\n");
 
-for(int i=0;i< numberOfList.length;i++){
-  print(numberOfList[i]);
-}
+  for (int i = 0; i < numberOfList.length; i++) {
+    print(numberOfList[i]);
+  }
 }
